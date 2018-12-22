d6d5f23599a85830f675c93d98751c97822314cb
Martin <martin.khannouz@gmail.com>
2018-12-22 12:29:29 -0400

    Modify to make tests work.

--- tests/test_count.py
+++ tests/test_count.py
@@ -1,10 +1,8 @@
 import subprocess
+import sys
+sys.path.insert(0, './answers')
+from answer import count
 
 def test_count():
-    command="python ./answers/count.py ./data/frenepublicinjection2016.csv"
-    process = subprocess.Popen(command, shell=True,
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE)
-    code=process.wait()
-    assert(not code), "Command failed"
-    assert(process.stdout.read().decode("utf-8")=='27244\n')
+    a = count("./data/frenepublicinjection2016.csv")
+    assert(a == 27244)
