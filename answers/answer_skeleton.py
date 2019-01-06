cd28cc4f37873f5f57c8f12217e063cbed3144b1
Martin <martin.khannouz@gmail.com>
2019-01-06 07:21:49 -0400

    Fill answer_skeleton.py

---
+++ answers/answer_skeleton.py
@@ -0,0 +1,212 @@
+import os
+import sys
+from pyspark.rdd import RDD
+from pyspark.sql import DataFrame
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import desc
+
+#Initialize a spark session
+spark = SparkSession \
+    .builder \
+    .appName("Python Spark SQL basic example") \
+    .config("spark.some.config.option", "some-value") \
+    .getOrCreate()
+
+#Useful functions to print RDDs and Dataframes.
+def toCSVLineRDD(rdd):
+    '''
+    This function convert an RDD or a DataFrame into a CSV string
+    '''
+    a = rdd.map(lambda row: ",".join([str(elt) for elt in row]))\
+           .reduce(lambda x,y: "\n".join([x,y]))
+    return a + "\n"
+def toCSVLine(data):
+    '''
+    Convert an RDD or a DataFrame into a CSV string
+    '''
+    if isinstance(data, RDD):
+        return toCSVLineRDD(data)
+    elif isinstance(data, DataFrame):
+        return toCSVLineRDD(data.rdd)
+    return None
+
+#Python answer functions
+def count(filename):
+    '''
+    Write a Python (not DataFrame, nor RDD) script that prints the number of trees (non-header lines) in
+    the data file passed as first argument.
+    Test file: tests/test_count.py
+    Note: The return value should be an integer
+    '''
+    return 0
+def parks(filename):
+    '''
+    Write a Python (not DataFrame, nor RDD) script that prints the number of trees that are *located in a park*.
+    To get the park location information, have a look at the *Nom_parc* column (name of park).
+    Test file: tests/test_parks.py
+    Note: The return value should be an integer
+    '''
+    return 0
+def uniq_parks(filename):
+    '''
+    Write a Python (not DataFrame, nor RDD) script that prints the list of unique parks where trees
+    were treated. The list must be ordered alphabetically. Every element in the list must be printed on
+    a new line.
+    Test file: tests/test_uniq_parks.py
+    Note: The return value should be a CSV string
+    '''
+    return "\n"
+def uniq_parks_counts(filename):
+    '''
+    Write a Python (not DataFrame, nor RDD) script that counts the number of trees treated in each park
+    and prints a list of "park,count" pairs in a CSV manner ordered
+    alphabetically by the park name. Every element in the list must be printed
+    on a new line.
+    Test file: tests/test_uniq_parks_counts.py
+    Note: The return value should be a CSV string
+          Have a look at the file *tests/list_parks_count.txt* to get the exact return format.
+    '''
+    return "\n"
+def frequent_parks_count(filename):
+    '''
+    Write a Python (not DataFrame, nor RDD) script that prints the list of the 10 parks with the
+    highest number of treated trees. Parks must be ordered by decreasing
+    number of treated trees and by alphabetical order when they have similar number.
+    Every list element must be printed on a new line.
+    Test file: tests/test_frequent_parks_count.py
+    Note: The return value should be a CSV string.
+          Have a look at the file *tests/frequent.txt* to get the exact return format.
+    '''
+    return "\n"
+def intersection(filename1, filename2):
+    '''
+    Write a Python (not DataFrame, nor RDD) script that prints the alphabetically sorted list of
+    parks that had trees treated both in 2016 and 2015. Every list element
+    must be printed on a new line.
+    Test file: tests/test_intersection.py
+    Note: The return value should be a CSV string.
+          Have a look at the file *tests/intersection.txt* to get the exact return format.
+    '''
+    return "\n"
+
+#DataFrame answer function
+#From now on, you will have to re-implement the previous functions using DataFrames.
+#Have a look at ASSIGNMENT.md for more instructions.
+def count_df(filename):
+    '''
+    Write a Python script using DataFrames that prints the number of trees
+    (non-header lines) in the data file passed as first argument.
+    Test file: tests/test_count.py
+    Note: The return value should be an integer
+    '''
+    return "\n"
+def parks_df(filename):
+    '''
+    Write a Python script using DataFrames that prints the number of trees that are *located in a park*.
+    To get the park location information, have a look at the *Nom_parc* column (name of park).
+    Test file: tests/test_parks.py
+    Note: The return value should be an integer
+    '''
+    return "\n"
+def uniq_parks_df(filename):
+    '''
+    Write a Python script using DataFrames that prints the list of unique parks
+    where trees were treated. The list must be ordered alphabetically. Every
+    element in the list must be printed on a new line.
+    Test file: tests/test_uniq_parks.py
+    Note: The return value should be a CSV string
+    '''
+    return "\n"
+def uniq_parks_counts_df(filename):
+    '''
+    Write a Python script using DataFrames that counts the number of trees
+    treated in each park and prints a list of "park,count" pairs in a CSV
+    manner ordered alphabetically by the park name. Every element in the list
+    must be printed on a new line.
+    Test file: tests/test_uniq_parks_counts.py
+    Note: The return value should be a CSV string
+          Have a look at the file *tests/list_parks_count.txt* to get the exact return format.
+    '''
+    return "\n"
+def frequent_parks_count_df(filename):
+    '''
+    Write a Python script using DataFrames that prints the list of the 10 parks
+    with the highest number of treated trees. Parks must be ordered by
+    decreasing number of treated trees and by alphabetical order when they have
+    similar number.  Every list element must be printed on a new line.
+    Test file: tests/test_frequent_parks_count.py
+    Note: The return value should be a CSV string.
+          Have a look at the file *tests/frequent.txt* to get the exact return format.
+    '''
+    return "\n"
+def intersection_df(filename1, filename2):
+    '''
+    Write a Python script using DataFrames that prints the alphabetically
+    sorted list of parks that had trees treated both in 2016 and 2015. Every
+    list element must be printed on a new line.
+    Test file: tests/test_intersection.py
+    Note: The return value should be a CSV string.
+          Have a look at the file *tests/intersection.txt* to get the exact return format.
+    '''
+    return "\n"
+
+#RDD answer function
+#From now on, you will have to re-implement the previous functions using RDDs.
+#Have a look at ASSIGNMENT.md for more instructions.
+def count_rdd(filename):
+    '''
+    Write a Python script using RDDs that prints the number of trees
+    (non-header lines) in the data file passed as first argument.
+    Test file: tests/test_count.py
+    Note: The return value should be an integer
+    '''
+    return "\n"
+def parks_rdd(filename):
+    '''
+    Write a Python script using RDDs that prints the number of trees that are *located in a park*.
+    To get the park location information, have a look at the *Nom_parc* column (name of park).
+    Test file: tests/test_parks.py
+    Note: The return value should be an integer
+    '''
+    return "\n"
+def uniq_parks_rdd(filename):
+    '''
+    Write a Python script using RDDs that prints the list of unique parks where
+    trees were treated. The list must be ordered alphabetically. Every element
+    in the list must be printed on a new line.
+    Test file: tests/test_uniq_parks.py
+    Note: The return value should be a CSV string
+    '''
+    return "\n"
+def uniq_parks_counts_rdd(filename):
+    '''
+    Write a Python script using RDDs that counts the number of trees treated in
+    each park and prints a list of "park,count" pairs in a CSV manner ordered
+    alphabetically by the park name. Every element in the list must be printed
+    on a new line.
+    Test file: tests/test_uniq_parks_counts.py
+    Note: The return value should be a CSV string
+          Have a look at the file *tests/list_parks_count.txt* to get the exact return format.
+    '''
+    return "\n"
+def frequent_parks_count_rdd(filename):
+    '''
+    Write a Python script using RDDs that prints the list of the 10 parks with
+    the highest number of treated trees. Parks must be ordered by decreasing
+    number of treated trees and by alphabetical order when they have similar
+    number.  Every list element must be printed on a new line.
+    Test file: tests/test_frequent_parks_count.py
+    Note: The return value should be a CSV string.
+          Have a look at the file *tests/frequent.txt* to get the exact return format.
+    '''
+    return "\n"
+def intersection_rdd(filename1, filename2):
+    '''
+    Write a Python script using RDDs that prints the alphabetically sorted list
+    of parks that had trees treated both in 2016 and 2015. Every list element
+    must be printed on a new line.
+    Test file: tests/test_intersection.py
+    Note: The return value should be a CSV string.
+          Have a look at the file *tests/intersection.txt* to get the exact return format.
+    '''
+    return "\n"
