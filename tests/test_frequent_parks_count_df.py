61e86ef608c8fe0c1024d2ec591812bcc41a4bfb
Tristan Glatard <tristan.glatard@concordia.ca>
2019-01-11 20:51:19 -0500

    Typos in DataFrame test names

--- tests/test_frequent_parks_count_df.py
+++ tests/test_frequent_parks_count_df.py
@@ -2,6 +2,6 @@ import sys
 sys.path.insert(0, './answers')
 from answer import frequent_parks_count_df
 
-def test_frequent_parks_count():
+def test_frequent_parks_count_df():
     a = frequent_parks_count_df("./data/frenepublicinjection2016.csv")
     assert(a == open("tests/frequent.txt","r").read())
